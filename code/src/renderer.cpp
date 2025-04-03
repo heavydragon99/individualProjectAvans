418ebe2b883aeb52dc5c926123f3fde1d7e23ded
Sean <heavydragon99@outlook.com>
2025-04-03 15:46:45 +0200

    Removed minimum speed for blue color

--- code/src/renderer.cpp
+++ code/src/renderer.cpp
@@ -117,7 +117,7 @@ sf::View Renderer::getLetterboxView(const sf::Vector2u &gameSize, int windowWidt
 void Renderer::drawParticles(const ParticleSystem &particleSystem)
 {
     sf::Vector2f maxVelocity = {150.0f, 150.0f};
-    sf::Vector2f minVelocity = {20.0f, 20.0f};
+    sf::Vector2f minVelocity = {0.0f, 0.0f};
 
     float maxSpeedMagnitude = std::sqrt(maxVelocity.x * maxVelocity.x + maxVelocity.y * maxVelocity.y);
     float minSpeedMagnitude = std::sqrt(minVelocity.x * minVelocity.x + minVelocity.y * minVelocity.y);
